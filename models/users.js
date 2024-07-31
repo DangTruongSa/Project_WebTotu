225df00f88621dbbdfdcc36f4209e8a8f2af7ef9
DangTruongSa <dasoaiwk@gmail.com>
2024-07-31 16:06:48 +0700

    giao dien bai post

--- models/users.js
+++ models/users.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
-    id: {type: ObjectId},
-    username: {type: String, required: true},
-    email: { type: String, required: true},
-    password: {type: String, required: true},
-    joindate: {type: Date, default: Date.now},
-    role: {type: Number, default: 1},// 1 = user / 2 admin viết bài / 3 ad quản lý toàn bộ
-    status: {type: Number, default: 1},// 1 trạng thái bình thường // 0 trạng thái bị khóa 
-    avatar: {type: String, default: "dfd"}
+    id: { type: ObjectId },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    joindate: { type: Date, default: Date.now },
+    role: { type: Number, default: 1 },// 1 = user / 2 admin
+    status: { type: Number, default: 1 },// 1 trạng thái bình thường // 0 trạng thái bị khóa 
+    avatar: { type: String, default: "dfd" }
 });
 
 
