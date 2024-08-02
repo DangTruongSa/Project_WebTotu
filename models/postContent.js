71fdd9030a5ab7c27ddac9a08bc0cfd477a28f5b
DangTruongSa <dasoaiwk@gmail.com>
2024-08-02 09:12:01 +0700

    Merge branch 'master' of https://github.com/DangTruongSa/Project_WebTotu

--- models/postContent.js
+++ models/postContent.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const postContentSchema = new Schema({
-    postId:{type: ObjectId, ref:'posts'},
-    title:{type:String },
-    content:{type: String },
-    image:{type: String, default: "111"}
+    postId: { type: ObjectId, ref: 'posts' },
+    title: { type: String },
+    content: { type: String },
+    image: { type: String, default: "111" }
 
 });
 
 
-module.exports = mongoose.model('postContent', postContentSchema)
+module.exports = mongoose.model('posts', postContentSchema)
